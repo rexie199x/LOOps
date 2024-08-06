82c0e9cfb129a176d02540361c53a30ec100a23e
rexie199x <celeriorex@gmail.com>
2024-08-06 13:21:50 +0800

    Update Ops_Process.py

--- Ops_Process.py
+++ Ops_Process.py
@@ -410,7 +410,7 @@ def main():
         unsafe_allow_html=True
     )
     
-    menu_options = ["Dashboard", "General Processes", "Discord Related Processes", "Templates", "Links", "Checklist"]
+    menu_options = ["Dashboard", "General Processes", "Discord Related Processes", "Templates", "Links"]
     choice = st.sidebar.radio("Go to", menu_options, index=0)  # Default to "Dashboard" page
 
     if choice == "Dashboard":
